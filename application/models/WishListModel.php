681be66e417caae1c4eaa8ed5fa2679b0c25221e
jake <magress1199@yahoo.com>
2021-12-01 21:55:51 +0800

    wishlist changes

--- application/models/WishListModel.php
+++ application/models/WishListModel.php
@@ -10,9 +10,19 @@ class WishListModel extends CI_Model{
                 "product_id" => $payload->productId,
                   
             );
+            $query = $this->db->query("SELECT product_id, email FROM cart WHERE product_id = $payload->productId AND email = '$payload->email'");
 
-            $this->db->insert('wishlist', $wishlist);
-
+            if($query->num_rows() > 0 && isset($payload->removeItem)){
+                $this->db->where('product_id', $payload->productId)->where('email', $payload->email);
+                $this->db->delete('wishList');
+            }else if($query->num_rows() > 0 && !isset($payload->removeItem)){
+               
+                $this->db->insert('wishlist', $wishlist);
+            }
+            else{
+                $this->db->insert('wishlist', $wishlist); 
+            }
+            
             $response = array(
                 "status" => "Success",
                 "message" => "wishlist Created"
