7ba00c8e9dd2fde4522aaa16d6cd70f2ea055452
jheymie <jheymie.lintingco@gmail.com>
2022-02-05 17:13:04 +0800

    modal added

--- application/views/pages/buyer_profile/profile.php
+++ application/views/pages/buyer_profile/profile.php
@@ -28,13 +28,42 @@
                 </div>
                 <div class="col-lg-12">
                     <div class="d-flex">
-                        <button class="btn btn-sm btn-primary">Edit Profile</button>
+                        <button class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop" >Edit Profile</button>
                         <button class="btn btn-sm btn-primary ms-3">Change Password</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Edit Profile</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="row g-3">
+            <div class="col-sm-12 col-lg-6">
+                <input type="text" class="form-control-sm form-control" placeholder= "Full Name">
+            </div>
+            <div class="col-sm-12 col-lg-6">
+                <input type="text" class="form-control-sm form-control" placeholder= "Email Address">
+            </div>
+            <div class="col-sm-12 col-lg-6">
+                <input type="text" class="form-control-sm form-control" placeholder= "Mobile Number">
+            </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary btn-sm" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary btn-sm">Save</button>
+      </div>
+    </div>
+  </div>
 </div>
 <script>
     $(document).ready(function () {
